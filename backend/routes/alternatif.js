1d873029232579081a2a7b4fb5d5beae845dce38
Faiq Rafii <rafiiwahyudi25@gmail.com>
2025-11-10 16:17:19 +0700

    f

--- backend/routes/alternatif.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const router=express.Router()
-const { getAllAlternatif, addAlternatif, updateAlternatif, deleteAlternatif, getTotalAlternatif } = require('../controllers/alternatifController')
-
-router.route('/').post(addAlternatif).get(getAllAlternatif).put(updateAlternatif).delete(deleteAlternatif)
-router.get('/total',getTotalAlternatif)
-
-module.exports=router
